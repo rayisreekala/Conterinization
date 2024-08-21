f432682027db211aba4c2e77fb4391ee18d520b2
Sreekala <sreekalarayi67@gmail.com>
2024-08-21 16:40:50 +0530

    commited successfully

---
+++ Photograph/src/main/java/com/example/demo/Camera/Cameramen.java
@@ -0,0 +1,42 @@
+package com.example.demo.Camera;
+
+public class Cameramen {
+	private String name;
+	private String image;
+	private int posts;
+	private int rating;
+	private String location;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public int getPosts() {
+		return posts;
+	}
+	public void setPosts(int posts) {
+		this.posts = posts;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+
+}
